6d1fbfa57e1b56874a43a5d93fd2bc45dbe9e75c
Jason <conqueror89107@gmail.com>
2021-04-26 15:27:16 +0800

    loop_basic

---
+++ loop-basic.py
@@ -0,0 +1,16 @@
+# while 迴圈
+# n = 1
+# sum = 0
+# while n <= 10:
+#     sum = sum + n
+#     n+=1
+# print(sum)
+
+# for 迴圈
+# for x in [3,5,1]: # 3 5 1
+# for x in "Hello": # H e l l o
+# for x in range(5,10): # 包含開頭不包含結束 -> 5 6 7 8 9
+sum = 0
+for x in range(5,10): # 包含開頭不包含結束 -> 5 6 7 8 9
+    sum = sum + x
+    print(sum)
